ea5f2d011ae7e55800e440d48245340a9e94acd7
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-12-04 11:30:38 +0900

    state상태가 변경될때마다 현재state를 출력

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -18,6 +18,7 @@ export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
   });
+  console.log("AuthContext state:", state);
 
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
