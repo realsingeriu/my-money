5520c3ee75aa7b57ff08b72c526a655ddf9b38c9
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-12-04 14:30:43 +0900

    네브바에 인증된경우 로그아웃만 보고 인증안된은가입하기보이게

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,26 +2,38 @@ import { Link } from "react-router-dom";
 // CSS 모듈을 사용하면 CSS클래스가 다른 컴포넌트에도 적용되는 것을 방지
 import styles from "./Navbar.module.css";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 export default function Navbar() {
   // useLogout 훅 호출 및 logout 함수 추출
   const { logout } = useLogout();
+  const { user } = useAuthContext();
+
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>myMoney</li>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">로그인</Link>
+            </li>
+            <li>
+              <Link to="/signup">가입하기</Link>
+            </li>
+          </>
+        )}
 
-        <li>
-          <Link to="/login">로그인</Link>
-        </li>
-        <li>
-          <Link to="/signup">가입하기</Link>
-        </li>
-        <li>
-          <button className="btn" onClick={logout}>
-            로그아웃
-          </button>
-        </li>
+        {user && (
+          <>
+            <li>안녕하세요, {user.displayName}</li>
+            <li>
+              <button className="btn" onClick={logout}>
+                로그아웃
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
