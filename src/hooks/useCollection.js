1e8e01d28ca181d5b5609b5bf4a687e884a3da3c
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-12-05 09:52:30 +0900

    파이어스토어 쿼리를 이용한 인증된 유저와 같은uid의 문서 가져오기

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,12 +1,17 @@
 import { useEffect, useState } from "react";
 import { firedb } from "../firebase/config";
 
-export const useCollection = (collection) => {
+export const useCollection = (collection, query) => {
   const [documents, setDocuments] = useState(null);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     let ref = firedb.collection(collection);
+
+    //두번째 파라미터 즉 query가 있으면
+    if (query) {
+      ref = ref.where(...query); //...으로 배열을 각각의 값으로 변환 [1,2,3] => (1,2,3)
+    }
 
     const unsub = ref.onSnapshot(
       (snapshot) => {
