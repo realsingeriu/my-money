bb389b7e186b46a9d393b09f25a33831355ecade
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-12-05 15:32:02 +0900

    footer 문의사항 내용 수정

--- src/pages/footer/footer.jsx
+++ src/pages/footer/footer.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import styles from "../footer/footer.module.css";
+import { FaGithub } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
 
 const Footer = () => {
+  const email = "realsingeriu12@naver.com";
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
@@ -19,15 +22,28 @@ const Footer = () => {
           </p>
         </div>
         <div className={styles.contact}>
-          <h4>문의사항</h4>
-          <p>Email: realsingeriu12@naver.com</p>
-          <a
-            href="https://www.github.com/realsingeriu/my-money"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </a>
+          <h2>문의사항</h2>
+          <p style={{ fontSize: "30px" }}>
+            Email:
+            <a href={`mailto:${email}`}>
+              <IoMdMail
+                style={{ fontSize: "30px", color: "white" }}
+                className="icon"
+              />
+            </a>
+          </p>
+          <p style={{ fontSize: "30px" }}>
+            깃허브 링크:
+            <a
+              href="https://www.github.com/realsingeriu/my-money"
+              target="_blank"
+            >
+              <FaGithub
+                className="icon"
+                style={{ fontSize: "30px", color: "white" }}
+              />
+            </a>
+          </p>
           <p>전화번호: 123-456-7890</p>
         </div>
       </div>
