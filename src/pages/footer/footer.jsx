9dffe925be4f6c63083625518b0471833558203d
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-12-05 14:46:15 +0900

    footer에 내용 추가

--- src/pages/footer/footer.jsx
+++ src/pages/footer/footer.jsx
@@ -8,6 +8,15 @@ const Footer = () => {
         <div className={styles.info}>
           <h3>My Money App</h3>
           <p>자신의 품목별 거래장부를 써봐요.</p>
+          <p>
+            firebase, firestore로 만든 my money app
+            <li>1. 네비게이션 가드 </li>
+            <li>2. 인증 컨텍스트 (Auth Context)</li>
+            <li>3. Hook 훅</li>
+            <li>4. query쿼리를 이용한 uid 출력</li>
+            <li>5. sortTransactions정렬 추가 </li>
+            <li>6. calculateTotal 거래 가격 총합</li>
+          </p>
         </div>
         <div className={styles.contact}>
           <h4>문의사항</h4>
